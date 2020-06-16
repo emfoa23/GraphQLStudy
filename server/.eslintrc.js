4e9a21e00fdca7fed09ce363ea07feb1e341af43
byungwook-ahn <byungwook.ahn@navercorp.com>
2020-06-17 03:36:49 +0900

    서버 기본설정

---
+++ server/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'log'],
+      },
+    ],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ObjectExpression: 1,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': [
+      'error',
+      {
+        args: 'none',
+        ignoreRestSiblings: true,
+        vars: 'all',
+      },
+    ],
+  },
+};
