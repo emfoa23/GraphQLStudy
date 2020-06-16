eb0a9da439cd6bd75e1aed7f1b906e22faaddd88
byungwook-ahn <byungwook.ahn@navercorp.com>
2020-06-17 05:02:57 +0900

    datasource 적용

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,17 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(({ url }) => {
   console.log(`url : ${url}`);
