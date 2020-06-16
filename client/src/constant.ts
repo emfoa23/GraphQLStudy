445c14484597d3a002e21c352f4470607447b7a5
byungwook-ahn <byungwook.ahn@navercorp.com>
2020-06-17 06:45:00 +0900

    client 마크업 가져옴

---
+++ client/src/constant.ts
@@ -0,0 +1,5 @@
+export const BODY_STYLE = {
+  BACKGROUND_COLOR: '#6200ea',
+  FONT_SIZE: '0.88rem',
+  COLOR: '#495057',
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+
 import App from './App';
+
+// const cache = new InMemoryCache();
+// const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+//   cache,
+//   link: new HttpLink({
+//     uri: 'http://localhost:4000/graphql',
+//     headers: {
+//       authorization: localStorage.getItem('token'),
+//       'client-name': 'Space Explorer [web]',
+//       'client-version': '1.0.0',
+//     },
+//   }),
+// });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  // <ApolloProvider client={client}>
+  //   <App />
+  // </ApolloProvider>,
+  document.getElementById('root'),
 );
